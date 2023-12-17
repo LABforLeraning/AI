c8b9fed4d98140c590df1e6733fdbd2b2ee8e073
Panthakit totid <koontyza007@gmail.com>
2023-12-17 09:13:29 +0700

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,15 @@ feature_name2 = 'sex'
 
 colors=[[1,0,0],[0,1,0]]
 
+# plt.plot(species_data.index, species_data[feature_name2], marker='o', linestyle='', color=colors[i], label=species)
+
+
+fig, axes = plt.subplots(nrows=1, ncols=2)
 for i, species in enumerate(info_classes):
    species_data = info[info['num'] == species]
-   plt.plot(species_data.index, species_data[feature_name1], marker='o', linestyle='', color=colors[i], label=species)
-   plt.plot(species_data.index, species_data[feature_name2], marker='o', linestyle='', color=colors[i], label=species)
+#    plt.plot(species_data.index, species_data[feature_name1], marker='o', linestyle='', color=colors[i], label=species)
+   axes[0].plot(species_data.index, species_data[feature_name1])
+axes[0].set_title('Age')
+
+plt.tight_layout()
+plt.show()
